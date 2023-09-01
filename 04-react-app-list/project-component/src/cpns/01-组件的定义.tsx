191318647af88bb5f1ce7361f51e63b11168e471
tutu <t433567423@163.com>
2023-09-01 09:44:52 +0800

    feat

---
+++ 04-react-app-list/project-component/src/cpns/01-组件的定义.tsx
@@ -0,0 +1,17 @@
+// 类组件
+
+import { Component } from 'react'
+export default class App extends Component {
+	constructor(props: any) {
+		super(props)
+		this.state = {}
+	}
+
+	render() {
+		return (
+			<div>
+				<h2>hallo react</h2>
+			</div>
+		)
+	}
+}
