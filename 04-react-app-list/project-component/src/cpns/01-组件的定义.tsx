c5a499f8bae6ed4aba8b038ea2bee89c442db58f
tutu <t433567423@163.com>
2023-09-01 09:47:52 +0800

    feat: 函数式组件

--- 04-react-app-list/project-component/src/cpns/01-组件的定义.tsx
+++ 04-react-app-list/project-component/src/cpns/01-组件的定义.tsx
@@ -1,17 +1,25 @@
 // 类组件
 
-import { Component } from 'react'
-export default class App extends Component {
-	constructor(props: any) {
-		super(props)
-		this.state = {}
-	}
+// import { Component } from 'react'
+// export default class App extends Component {
+// 	constructor(props: any) {
+// 		super(props)
+// 		this.state = {}
+// 	}
 
-	render() {
-		return (
-			<div>
-				<h2>hallo react</h2>
-			</div>
-		)
-	}
+// 	render() {
+// 		return (
+// 			<div>
+// 				<h2>hallo Class component</h2>
+// 			</div>
+// 		)
+// 	}
+// }
+
+export default function App() {
+	return (
+		<div>
+			<h2>hallo Function component</h2>
+		</div>
+	)
 }
