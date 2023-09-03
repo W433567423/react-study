eeeaf77bc59ac0e4c4eb6215129ac08fa090ecb9
tutu <t433567423@163.com>
2023-09-04 07:53:30 +0800

    提交缓冲区文件

--- git.bat
+++ git.bat
@@ -17,37 +17,37 @@ if  "%commit%" == "" (set "commit=提交缓冲区文件")
 
 @echo %ESC_CHAR%[38;5;2m开始执行脚本%ESC_CHAR%[m
 @echo %ESC_CHAR%[38;5;226m将文件添加到缓冲区...%ESC_CHAR%[m
-@echo %ESC_CHAR%[38;5;3m%%>git add . ing...%ESC_CHAR%[m
+@echo %ESC_CHAR%[38;5;3m^>git add . ing...%ESC_CHAR%[m
 %gitPath% add . >nul
 
 @echo.
 @echo %ESC_CHAR%[38;5;226m将文件提交到远程仓库...%ESC_CHAR%[m
-@echo %ESC_CHAR%[38;5;3m%%>git commit -m "%commit%" ing...%ESC_CHAR%[m
+@echo %ESC_CHAR%[38;5;3m^>git commit -m "%commit%" ing...%ESC_CHAR%[m
 %gitPath% commit -m "%commit%"
 
 @echo.
 @echo %ESC_CHAR%[38;5;226m推送到%branchName%分支...%ESC_CHAR%[m
-@echo %ESC_CHAR%[38;5;3m%%>git push origin powo ing...%ESC_CHAR%[m
+@echo %ESC_CHAR%[38;5;3m^>git push origin powo ing...%ESC_CHAR%[m
 %gitPath% push origin powo
 
 @echo.
 @echo %ESC_CHAR%[38;5;226m切换到main分支...%ESC_CHAR%[m
-@echo %ESC_CHAR%[38;5;3m%%>git checkout main ing...%ESC_CHAR%[m
+@echo %ESC_CHAR%[38;5;3m^>git checkout main ing...%ESC_CHAR%[m
 %gitPath% checkout main
 
 @echo.
 @echo %ESC_CHAR%[38;5;226mmain分支合并%branchName%分支...%ESC_CHAR%[m
-@echo %ESC_CHAR%[38;5;3m%%>git merge origin/powo ing...%ESC_CHAR%[m
+@echo %ESC_CHAR%[38;5;3m^>git merge origin/powo ing...%ESC_CHAR%[m
 %gitPath% merge origin/powo
 
 @echo.
 @echo %ESC_CHAR%[38;5;226m推送到main分支...%ESC_CHAR%[m
-@echo %ESC_CHAR%[38;5;3m%%>git push origin main ing...%ESC_CHAR%[m
+@echo %ESC_CHAR%[38;5;3m^>git push origin main ing...%ESC_CHAR%[m
 %gitPath% push origin main
 
 @echo.
 @echo %ESC_CHAR%[38;5;226m切换到%branchName%分支...%ESC_CHAR%[m
-@echo %ESC_CHAR%[38;5;3m%%>git checkout powo ing...%ESC_CHAR%[m
+@echo %ESC_CHAR%[38;5;3m^>git checkout powo ing...%ESC_CHAR%[m
 %gitPath% checkout powo
 
 @echo.
