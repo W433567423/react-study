1cf7fcfa6baea1dc0838d85a771dab0ca726e85e
tutu <t433567423@163.com>
2023-09-05 09:44:18 +0800

    feat: react css

---
+++ 05-react_style/src/cpns/01-内联样式.tsx
@@ -0,0 +1,15 @@
+import React, {PureComponent} from 'react';
+
+class App
+    extends PureComponent {
+    render() {
+        return (
+            <div>
+                <p style={{color: "red", fontSize: "20px", fontWeight: '600'}}>内联样式</p>
+            </div>
+        );
+    }
+}
+
+
+export default App;
